40ead42c341fefbcb48fb00e6fc6d71bfa319e20
andririzkisaputra <andri.rizki007@gmail.com>
2025-01-07 23:12:16 +0700

    update data

--- app/Database/Seeds/PenyakitSeeder.php
+++ app/Database/Seeds/PenyakitSeeder.php
@@ -14,38 +14,38 @@ class PenyakitSeeder extends Seeder
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'cloudy eye',
-                'solusi' => "Dikarantina dengan heater dengan suhu 34 dan di beri obat jamur insang",
-                'img' => "jamur-insang.png"
+                'solusi' => "Perbaiki kualitas air dengan mengganti sebagian air. Gunakan antibiotik atau larutan garam untuk membantu pemulihan. Pastikan pH air stabil.",
+                'img' => "cloudy-eye.png"
             ],
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'kutu jangkar',
-                'solusi' => "Dikarantina dengan suhu 34 dan di beri garam grosok dan tambahkan metlyn blue",
-                'img' => "white-spot.png"
+                'solusi' => "Gunakan obat anti-parasit yang khusus untuk kutu ikan, seperti formalin atau tembaga sulfat. Pastikan untuk mengikuti dosis yang tepat.",
+                'img' => "kutu-jangkar.png"
             ],
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'black spot',
-                'solusi' => "Dikarantina dengan suhu 34 dan di beri obat antibiotic(amoxicilin)",
-                'img' => "aeromonas.png"
+                'solusi' => "Penyakit ini biasanya disebabkan oleh parasit atau infeksi jamur. Gunakan obat anti-jamur atau parasit, serta pastikan kualitas air terjaga.",
+                'img' => "black-spot.png"
             ],
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'white spot',
-                'solusi' => "Dikarantina dengan suhu 34 dan di beri obat antibiotic(amoxicilin) serta air dalam karantina di kuras 70% setiap hari dan diberikan obat sampai sembuh",
-                'img' => "dropsy-sisik-nanas.png"
+                'solusi' => "Obat yang mengandung malachite green atau formalin dapat digunakan untuk mengobati infeksi parasit ini. Tingkatkan suhu air untuk mempercepat proses penyembuhan.",
+                'img' => "white-spot.png"
             ],
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'dropsy',
-                'solusi' => "Dikarantina, diberi garam grosok setelah 3 hari lalu di beri obat jamur (Metylyn Blue)",
-                'img' => "sirip-dan-ekor-gripis.png"
+                'solusi' => "Dropy disebabkan oleh infeksi bakteri yang mengakibatkan pembengkakan. Gunakan antibiotik dalam air atau langsung pada ikan. Pisahkan ikan yang sakit agar tidak menular.",
+                'img' => "dropsy.png"
             ],
             [
                 'kode_penyakit' => $code . $i++,
                 'nama' => 'fin/tail rot',
-                'solusi' => "Dikarantina dan diberi obat kutu (dengan dosis tinggi yang disesuaikan dengan tempat karantina) 1 gram untuk 1000L serta air diganti setiap 2 hari dan ulangi pemberian obat sampai terbebas dari kutu",
-                'img' => "kutu-kura-kura-argulus.png"
+                'solusi' => "Potong bagian sirip yang terinfeksi dan gunakan antiseptik atau antibiotik untuk mencegah infeksi lebih lanjut. Perbaiki kualitas air dan pastikan tidak ada polusi.",
+                'img' => "fin-tail-rot.png"
             ]
         ];
         $this->db->table('penyakit')->insertBatch($data);
